75dd248dc55312a2bce86e2c07e1053e7c27cd4e
Piotr Moroń <noperekq@gmail.com>
2021-07-20 08:49:07 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,36 +1,24 @@
 const rps = ['rock', 'paper', 'scissors']
-const gameItem = document.querySelectorAll('.game__item');
-const firstTitle = document.querySelector('.game-first-title');
-var result = document.querySelector('.score');
+const gameItems = document.querySelectorAll('.game__item');
+const firstTitle = document.querySelector('.game__first-title');
+const yourTurn = document.querySelector('.yourturn');
+const autoTurn = document.querySelector('.autoturn');
+const result = document.querySelector('.score');
 
-function draw(yourChoose, AutoChoose) {
-    if (yourChoose == AutoChoose) {
+function draw(yourChoice, AutoChoose) {
+    if (yourChoice == AutoChoose) {
         result.textContent = 'Remis !'
     } else {
         return;
     }
 }
 
-function winner(yourChoose, autoChoose) {
-    if (yourChoose !== autoChoose) {
-        if (yourChoose == 'rock' && autoChoose == 'scissors') {
+function winner(yourChoice, autoChoose) {
+    if (yourChoice !== autoChoose) {
+        if (yourChoice == 'rock' && autoChoose == 'scissors' || yourChoice == 'paper' && autoChoose == 'rock' || yourChoice == 'scissors' && autoChoose == 'paper' )  {
             result.textContent = 'Wygrałeś !'
         } else {
-            if (yourChoose == 'rock' && autoChoose == 'paper') {
-                result.textContent = 'Przegrałeś !'
-            }
-        }
-        if (yourChoose == 'paper' && autoChoose == 'rock') {
-            result.textContent = 'Wygrałeś !'
-        } else {
-            if (yourChoose == 'paper' && autoChoose == 'scissors') {
-                result.textContent = 'Przegrałeś !'
-            }
-        }
-        if (yourChoose == 'scissors' && autoChoose == 'paper') {
-            result.textContent = 'Wygrałeś !'
-        } else {
-            if (yourChoose == 'scissors' && autoChoose == 'rock') {
+            if (yourChoice == 'rock' && autoChoose == 'paper' || yourChoice == 'paper' && autoChoose == 'scissors' || yourChoice == 'scissors' && autoChoose == 'rock') {
                 result.textContent = 'Przegrałeś !'
             }
         }
@@ -39,10 +27,8 @@ function winner(yourChoose, autoChoose) {
     }
 }
 
-gameItem.forEach(el => {
+gameItems.forEach(el => {
     el.addEventListener('click', () => {
-        let yourTurn = document.querySelector('.yourturn');
-        let autoTurn = document.querySelector('.autoturn');
         firstTitle.remove();
         const dataItem = el.getAttribute('data-item');
         const random = rps[Math.floor(Math.random() * rps.length)];
@@ -50,6 +36,5 @@ gameItem.forEach(el => {
         autoTurn.textContent = random;
         winner(dataItem, random)
         draw(dataItem, random)
-        
     })
 })
